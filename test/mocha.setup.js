d7623b1c01f67e1dfc594da028dfb7b428f24905
Hombach <GIT@homba.ch>
2025-01-01 12:30:27 +0100

    1.0.1

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -14,16 +14,6 @@ process.on("unhandledRejection", e => {
 
 // enable the should interface with sinon
 // and load chai-as-promised and sinon-chai by default
-/*
-const sinonChai = require("sinon-chai");
-const chaiAsPromised = require("chai-as-promised");
-const { should, use } = require("chai");
-
-should();
-use(sinonChai);
-use(chaiAsPromised);
-*/
-
 const sinonChai = require("sinon-chai");
 const { should, use } = require("chai");
 
